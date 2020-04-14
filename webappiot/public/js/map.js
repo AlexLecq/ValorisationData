19ad29167b54eee97b31c3d46755b7aa64f060e4
AlexLecq <alex.lecq6@gmail.com>
2020-04-14 17:04:49 +0200

    pas besoin de temps réel (pas de socket) + init de la map

--- webappiot/public/js/map.js
+++ webappiot/public/js/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const mymap = L.map('mapid').setView([48.85, 2.35], 6);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,3 +8,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiYWxleGxlY3EiLCJhIjoiY2s4enhrajdqMDBjMjNlcXZqZzYzb3YzbyJ9.b7mcgopV5pSE4zTsqzVtOQ'
 }).addTo(mymap);
+
+var circle = L.circle([43.61, 3.88], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 50000
+}).addTo(mymap);
+
+
--- webappiot/src/server.js
+++ webappiot/src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const mqtt = require('mqtt');
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 const WeatherDataAccess = require('./dal/WeatherDataAccess');
 const weatherDataClient = new WeatherDataAccess(
     {
@@ -9,9 +10,9 @@ const weatherDataClient = new WeatherDataAccess(
         db: process.env.MONGODB_DB,
         col: process.env.MONGODB_COL
     });
-let client;
+let mqttClient;
 try{
-    client = mqtt.connect(process.env.MQTT_URL);
+    mqttClient = mqtt.connect(process.env.MQTT_URL);
 }catch (e) {
     console.error('un problème est survenue lors de la connexion au serveur MQTT : '+ e);
 }
@@ -20,13 +21,14 @@ app.use(express.json());
 app.use(express.static('public'));
 app.listen(process.env.PORT);
 
-client.on('connect', ()=>{
+
+mqttClient.on('connect', ()=>{
    console.log('Connection serveur MQTT => OK');
 });
 
-client.subscribe(process.env.MQTT_TOPIC, (err) => {
+mqttClient.subscribe(process.env.MQTT_TOPIC, (err) => {
     if(err) throw err;
-    client.on('message', (topic, weather) => {
+    mqttClient.on('message', (topic, weather) => {
         // message is Buffer
         if(!weather){
             console.error('Aucune donnée n\'a été récupéré');
