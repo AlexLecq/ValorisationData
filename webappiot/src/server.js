c19ac7921b26b097060bfdc0415a0136c8b82cc9
AlexLecq <alex.lecq6@gmail.com>
2020-04-16 08:38:13 +0200

    blabla

--- webappiot/src/server.js
+++ webappiot/src/server.js
@@ -1,59 +1,56 @@
-require('dotenv').config();
-const mqtt = require('mqtt');
-const express = require('express');
+require("dotenv").config();
+const mqtt = require("mqtt");
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const WeatherDataAccess = require('./dal/WeatherDataAccess');
-const weatherDataClient = new WeatherDataAccess(
-    {
-        url:process.env.MONGODB_URL,
-        db: process.env.MONGODB_DB,
-        col: process.env.MONGODB_COL
-    });
+const server = require("http").createServer(app);
+const WeatherDataAccess = require("./dal/WeatherDataAccess");
+const weatherDataClient = new WeatherDataAccess({
+  url: process.env.MONGODB_URL,
+  db: process.env.MONGODB_DB,
+  col: process.env.MONGODB_COL,
+});
 let mqttClient;
-try{
-    mqttClient = mqtt.connect(process.env.MQTT_URL);
-}catch (e) {
-    console.error('un problème est survenue lors de la connexion au serveur MQTT : '+ e);
+try {
+  mqttClient = mqtt.connect(process.env.MQTT_URL);
+} catch (e) {
+  console.error(
+    "un problème est survenue lors de la connexion au serveur MQTT : " + e
+  );
 }
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.listen(process.env.PORT);
 
-
-mqttClient.on('connect', ()=>{
-   console.log('Connection serveur MQTT => OK');
+mqttClient.on("connect", () => {
+  console.log("Connection serveur MQTT => OK");
 });
 
 mqttClient.subscribe(process.env.MQTT_TOPIC, (err) => {
-    if(err) throw err;
-    mqttClient.on('message', (topic, weather) => {
-        // message is Buffer
-        if(!weather){
-            console.error('Aucune donnée n\'a été récupéré');
-            return;
-        }
-        const weatherJson = JSON.parse(weather.toString());
-        console.dir(weatherJson);
-        weatherDataClient.insert(weatherJson);
-    });
+  if (err) throw err;
+  mqttClient.on("message", (topic, weather) => {
+    // message is Buffer
+    if (!weather) {
+      console.error("Aucune donnée n'a été récupéré");
+      return;
+    }
+    const weatherJson = JSON.parse(weather.toString());
+    console.dir(weatherJson);
+    weatherDataClient.insert(weatherJson);
+  });
 });
 
-app.get('/cities', async (req, res) => {
-    const startTime = req.query.startTime;
-    const endTime = req.query.endTime;
-    res.send(await weatherDataClient.getDataForPeriod(startTime,endTime));
+app.get("/cities", async (req, res) => {
+  const startTime = req.query.startTime;
+  const endTime = req.query.endTime;
+  res.send(await weatherDataClient.getDataForPeriod(startTime, endTime));
 });
 
-app.get('/city', async (req, res) => {
-    const startTime = req.query.startTime;
-    const endTime = req.query.endTime;
-    const cityName = req.query.cityName;
-    res.send(await weatherDataClient.getCityDataForPeriod(startTime,endTime,cityName));
+app.get("/city", async (req, res) => {
+  const startTime = req.query.startTime;
+  const endTime = req.query.endTime;
+  const cityName = req.query.cityName;
+  res.send(
+    await weatherDataClient.getCityDataForPeriod(startTime, endTime, cityName)
+  );
 });
-
-
-
-
-
